af5a31f0d6610ff770b55ce9100ac95f0724e0db
Humphrey <hlopez@brightlight-consulting.com>
2024-09-06 16:44:10 +0200

    Added configuration for datastorage.

--- server/src/test/kotlin/nl/ignite/kubernetes/demo/server/ServerApplicationTest.kt
+++ server/src/test/kotlin/nl/ignite/kubernetes/demo/server/ServerApplicationTest.kt
@@ -10,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import java.net.InetSocketAddress
 import javax.management.MBeanServer
 
-@SpringBootTest
+@SpringBootTest(properties = ["ignite.dataStorageConfiguration.defaultDataRegionConfiguration.initialSize=536870912", "ignite.dataStorageConfiguration.defaultDataRegionConfiguration.maxSize=1073741824"])
 class ServerApplicationTest {
 
     @Autowired
@@ -27,10 +27,30 @@ class ServerApplicationTest {
         SoftAssertions.assertSoftly {
             it.assertThat(ignite).isNotNull
             it.assertThat(ignite.cluster().localNode().isClient).isFalse
+            it.assertThat(ipFinder).isNotNull
+            it.assertThat(mBeanServer).isNotNull
+        }
+    }
+
+    @Test
+    fun testIpFinder() {
+        SoftAssertions.assertSoftly {
             it.assertThat(ipFinder).isInstanceOf(TcpDiscoveryVmIpFinder::class.java)
             it.assertThat(ipFinder.registeredAddresses).hasSize(10)
             it.assertThat(ipFinder.registeredAddresses).contains(InetSocketAddress("localhost", 47500))
-            it.assertThat(mBeanServer).isNotNull
+        }
+    }
+
+    @Test
+    fun testDataStorage() {
+        SoftAssertions.assertSoftly {
+            it.assertThat(ignite.configuration()).isNotNull
+            val dataStorageConfiguration = ignite.configuration().dataStorageConfiguration
+            it.assertThat(dataStorageConfiguration).isNotNull
+            val defaultDataRegion = dataStorageConfiguration.defaultDataRegionConfiguration
+            it.assertThat(defaultDataRegion).isNotNull
+            it.assertThat(defaultDataRegion.initialSize).isEqualTo(512 * 1024 * 1024)
+            it.assertThat(defaultDataRegion.maxSize).isEqualTo(1 * 1024 * 1024 * 1024)
         }
     }
 }
